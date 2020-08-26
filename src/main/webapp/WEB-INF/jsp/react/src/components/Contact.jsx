64b78d032bf65ef5b1d619a44372932ba82418e0
geo-gkez <georgegezeris@gmail.com>
2020-08-26 22:08:41 +0300

    front_end

---
+++ src/main/webapp/WEB-INF/jsp/react/src/components/Contact.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const style = {
+    objectFit: 'cover',
+    borderRadius: '50%',
+    width: '40px',
+    height: '40px'
+};
+
+function Contact(props) {
+
+    return (
+        <>
+            <div className="card my-2 border-0">
+                <div className="card-body d-flex flex-row pt-2 pb-0 px-1">
+                    <img style={style} src={props.avatar}
+                        className="avatar rounded-circle mx-2"
+                        alt="Cinque Terre" />
+                    <h6 className="card-text align-bottom mt-2">{props.username}</h6>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Contact;
---
+++ src/main/webapp/WEB-INF/jsp/react/src/pages/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+import ProfileBg from '../components/ProfileBg';
+
+import '../components/profile.css';
+
+
+
+export default function Profile() {
+    return (
+        <div>
+            <div className="row">
+                <div className="col-2"></div>
+                <div className="col-8">
+
+                    <ProfileBg />
+                </div>
+                <div className="col-2"></div>
+            </div>
+        </div>
+    )
+}
