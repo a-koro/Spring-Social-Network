52032f73fe7def982ef8bc42b843a0eac07c77f6
Aris Plexi <aris.plexi@gmail.com>
2020-08-27 02:39:47 +0300

    adding new files

--- src/main/webapp/WEB-INF/jsp/react/src/components/Contact.jsx
+++ src/main/webapp/WEB-INF/jsp/react/src/components/Contact.jsx
@@ -13,9 +13,9 @@ function Contact(props) {
         <>
             <div className="card my-2 border-0">
                 <div className="card-body d-flex flex-row pt-2 pb-0 px-1">
-                    <img style={style} src={props.avatar}
-                        className="avatar rounded-circle mx-2"
-                        alt="Cinque Terre" />
+                    {/*<img style={style} src={props.avatar}*/}
+                    {/*    className="avatar rounded-circle mx-2"*/}
+                    {/*    alt="Cinque Terre" />*/}
                     <h6 className="card-text align-bottom mt-2">{props.username}</h6>
                 </div>
             </div>
--- src/main/webapp/WEB-INF/jsp/react/src/components/NewsFeed.jsx
+++ src/main/webapp/WEB-INF/jsp/react/src/components/NewsFeed.jsx
@@ -21,7 +21,7 @@ function NewsFeed(props) {
     const [items, setItems] = React.useState([]);
 
     React.useEffect(() => {
-        fetch('https://api.jsonbin.io/b/5f23ffa2250d377b5dc791de')
+        fetch('http://localhost:8080/api/user')
             .then(response => response.json())
             .then(data => setItems(data));
     }, []
@@ -29,11 +29,10 @@ function NewsFeed(props) {
 
     return (
         <div className="row" style={style}>
-            <div className="col-3" style={styleBar}>
-                <div>{items.map((item) => (
-                    <Contact username={item.first_name + " " + item.last_name} avatar={item.avatar} />
-                ))}</div>
-            </div>
+            <div className="col-2" style={styleBar}>
+                {console.log(items)}
+                    <Contact username={items.firstName + " " + items.lastName} />
+                </div>
             <div className="col-9">
                 <Post username="Rick Sanchez" post="After having been missing for nearly 20 years, Rick Sanchez suddenly arrives at daughter Beth's doorstep to move in with her and her family. Although Beth welcomes Rick into her home, her husband, Jerry, isn't as happy about the family reunion." />
                 <Comment username="Alex Koro" post="After having been missing for nearly 20 years, Rick Sanchez suddenly arrives at daughter Beth's doorstep to move in with her and her family." />
