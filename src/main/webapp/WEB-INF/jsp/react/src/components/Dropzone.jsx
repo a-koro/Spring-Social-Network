8099c8087a55ecc1e673f8d7fa8168e72f4b51c5
geo-gkez <georgegezeris@gmail.com>
2020-08-27 13:28:52 +0300

    image upload not finish

---
+++ src/main/webapp/WEB-INF/jsp/react/src/components/Dropzone.jsx
@@ -0,0 +1,47 @@
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone';
+
+import axios from 'axios';
+
+function Dropzone({userid}) {
+    console.log("drop userid ",userid);
+    const onDrop = useCallback(acceptedFiles => {
+      const file = acceptedFiles[0];
+      console.log(file);
+      
+
+
+      const formData= new FormData();
+      formData.append("file",file);
+
+      axios.post(
+        `http://localhost:8080/api/profile/${userid}/image/upload`,
+        formData,
+        {
+            headers:{
+                "Content-Type":"multipart/form-data"
+            }
+        }
+      ).then(()=>{
+          console.log("file upload success")
+      }).catch(err=>{
+          console.log(err);
+      });
+        
+
+    }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  
+    return (
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <p>Drop the image here ...</p> :
+            <p>Drag 'n' drop profile image, or click to select profile image</p>
+        }
+      </div>
+    )
+  }
+
+export default Dropzone;
