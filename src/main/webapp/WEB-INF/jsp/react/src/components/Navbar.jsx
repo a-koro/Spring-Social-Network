b4082822075b054a845fcb34d551c59ed46a2e34
a-koro <korove@hotmail.com>
2020-08-28 14:55:22 +0300

    Created ResultUser component (larger avatar and username) Completed Context API to send data from navbar component to search component Added bootstrap classes to Search component

--- src/main/webapp/WEB-INF/jsp/react/src/components/Navbar.jsx
+++ src/main/webapp/WEB-INF/jsp/react/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
 import { useHistory, Link } from "react-router-dom";
 import DataServices from '../services/DataServices';
 
@@ -8,6 +7,9 @@ let results = ["Test DATA 001"];
 export const ResultsContext = React.createContext({});
 
 export const ResultsProvider = (props) => {
+
+    const [stateResults, setStateResults] = React.useState([]);
+
     return (
         <ResultsContext.Provider value={results}>
             {props.children}
@@ -70,12 +72,12 @@ function Navbar() {
     const [searchResults, setSearchResults] = React.useState([]);
 
     function getCurrentUser() {
-    //     DataServices.getCurrentUser()
-    //         .then(data => {
-    //             setUser(data.data);
-    //             console.log(data.data);
-    //         })
-    //     .catch(error => { console.log(error); });
+    // DataServices.getCurrentUser().then(
+    //     response => {
+    //         console.log("users: ", response.data);
+    //         setUser(response.data);
+    //     }
+    // );
     fetch("/userDetails", {
         method: 'GET',
         credentials: "include"
@@ -106,21 +108,24 @@ function Navbar() {
             .then(data => {
                 setSearchResults(data);
                 results = data;
-                console.log(data);
+                console.log(results[0].email);
             });
-        history.push("/results");
     }
 
+    const isInitialMount = React.useRef(true);
     React.useEffect(() => {
-        results = searchResults;
-        console.log(searchResults)
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+        } else {
+            history.push("/results");
+        }
     },[searchResults]);
 
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="#">
-                    <img src="http://placehold.it/150x50?text=Logo" width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy" />
+                    {/*<img src="http://placehold.it/150x50?text=Logo" width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy" />*/}
                     Connector
                 </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -129,7 +134,7 @@ function Navbar() {
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <form className="form-inline my-2 my-lg-0" onSubmit={fetchUsers}>
-                        <input id="searchBar" className="form-control mr-sm-2" name="search" type="search" placeholder="Search" aria-label="Search" />
+                        <input id="searchBar" className="form-control autocomplete mr-sm-2" name="search" type="search" placeholder="Search" aria-label="Search" />
                     </form>
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
@@ -147,7 +152,7 @@ function Navbar() {
                                  alt="Cinque Terre" />
                         </li>
                         <li className="nav-item">
-                            <Link to="/profile" className="nav-link">Ale</Link>
+                            <Link to="/profile" className="nav-link">{username}</Link>
                         </li>
                         <li>
                             <a className="nav-link" href="/logout">Logout</a>
--- src/main/webapp/WEB-INF/jsp/react/src/components/Search.jsx
+++ src/main/webapp/WEB-INF/jsp/react/src/components/Search.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { ResultsContext } from './Navbar';
+import ResultUser from './ResultUser';
 import context from 'react-bootstrap/esm/AccordionContext';
+import Contact from "./Contact";
 
 function Search(props) {
 
     return (
         <div>
-            <ResultsContext.Consumer>
-                {(context) => (
-                    <p>{context}</p>
-                )}
-            </ResultsContext.Consumer>
-            <h1>
-                Search Results
-            </h1>
+            <div className="col-sm-6 offset-sm-3 col-xs-12 mt-3">
+                <h4>
+                    Search results
+                </h4>
+                <ResultsContext.Consumer>
+                    {(context) => (
+                        context.map((item) => (
+                            <ResultUser username={item.firstName + " " + item.lastName} avatar="https://3.bp.blogspot.com/-qDc5kIFIhb8/UoJEpGN9DmI/AAAAAAABl1s/BfP6FcBY1R8/s1600/BlueHead.jpg" />
+                        ))
+                    )}
+                </ResultsContext.Consumer>
+            </div>
         </div>
     );
 }
