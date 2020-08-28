150b0c087b696386e03f10885f7cf62d6ff1f34f
geo-gkez <georgegezeris@gmail.com>
2020-08-28 12:30:18 +0300

    image upload

--- src/main/webapp/WEB-INF/jsp/react/src/components/Dropzone.jsx
+++ src/main/webapp/WEB-INF/jsp/react/src/components/Dropzone.jsx
@@ -3,7 +3,7 @@ import {useDropzone} from 'react-dropzone';
 
 import axios from 'axios';
 
-function Dropzone({val}) {
+function Dropzone() {
     const onDrop = useCallback(acceptedFiles => {
       const file = acceptedFiles[0];
       console.log(file);
