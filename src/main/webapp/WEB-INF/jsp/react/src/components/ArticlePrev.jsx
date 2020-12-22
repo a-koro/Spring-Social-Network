b0054381c2216217ac8b999e24f79c444b0c6432
a-koro <korove@hotmail.com>
2020-12-22 17:13:44 +0200

    each article now shows TimeAgo published

--- src/main/webapp/WEB-INF/jsp/react/src/components/ArticlePrev.jsx
+++ src/main/webapp/WEB-INF/jsp/react/src/components/ArticlePrev.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TimeAgo from 'react-timeago';
 import "../css/article.css";
 
 export default function ArticlePrev(props) {
@@ -11,6 +12,7 @@ export default function ArticlePrev(props) {
                     <h6 className="card-title">{props.article.title}</h6>
                     <div>
                         <span className="card-text float-left"><small className="text-muted">{"@" + props.article.user.firstName + " " + props.article.user.lastName}</small></span>
+                        <span className="card-text float-right"><small className="text-muted"><TimeAgo date={props.article.createdAt}/></small></span>
                     </div>
 
                 </div>
