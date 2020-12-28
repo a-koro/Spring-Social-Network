147bd5079b7b5a0d0e3caf7731ced96cadbb99e0
a-koro <korove@hotmail.com>
2020-12-28 20:17:19 +0200

    New repo method to get new comments on users posts

--- src/main/java/com/connector/beta/repos/CommentRepo.java
+++ src/main/java/com/connector/beta/repos/CommentRepo.java
@@ -1,12 +1,17 @@
 package com.connector.beta.repos;
 
 import com.connector.beta.entities.Comment;
+import com.connector.beta.entities.MyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepo extends JpaRepository<Comment, Integer> {
+
+    public List<Comment> findAllByPostUserAndUserNotAndViewedFalseOrderByCreatedAsc(MyUser user, MyUser user1);
 
     @Query("SELECT COUNT(c.commentId) FROM Comment c")
     public int getLengthOfComments();
