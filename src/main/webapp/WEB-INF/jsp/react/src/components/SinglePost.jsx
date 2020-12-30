2a25ee72a55bceaeccd2138256ddb3fff7272638
a-koro <korove@hotmail.com>
2020-12-30 20:04:02 +0200

    New SinglePost page to view a single post

---
+++ src/main/webapp/WEB-INF/jsp/react/src/components/SinglePost.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Post from "./Post";
+
+export default function SinglePost() {
+    return (
+        <>
+            <div className="col-md-6 offset-md-3 col-12">
+            </div>
+        </>
+    );
+}
