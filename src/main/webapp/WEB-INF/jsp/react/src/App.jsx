74be97ec5e984eb686b98c236df6b43c3bc681d6
a-koro <korove@hotmail.com>
2021-01-19 21:14:13 +0200

    new global state (context API)

--- src/main/webapp/WEB-INF/jsp/react/src/App.jsx
+++ src/main/webapp/WEB-INF/jsp/react/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
+import {Route, Switch, BrowserRouter, Redirect} from 'react-router-dom';
 import Navbar, {ResultsProvider, UserIdProvider} from './components/Navbar';
 import NewsFeed from './components/NewsFeed';
 import Search from './components/Search';
@@ -9,33 +9,46 @@ import FriendsProfilePage from "./pages/FriendsProfilePage";
 import Chat from "./pages/Chat";
 import ConnectionsPage from './pages/ConnectionsPage';
 import SinglePost from "./components/SinglePost";
+import MessengerContext from "./contexts/MessengerContext";
 
 const SearchContext = React.createContext({});
 
 function App(props) {
 
-  return (
+    const [koroUser, setKoroUser] = React.useState({});
+    const [activeChat, setActiveChat] = React.useState({});
+    const [chatMessages, setChatMessages] = React.useState([]);
 
-      <div className="container-fluid p-0">
-          <BrowserRouter>
-              <Navbar />
-              <div className="row m-0">
-              <Switch>
-                  <ResultsProvider>
-                      <Redirect from="/index.html" to="/" exact />
-                      <Route path="/results" component={Search}/>
-                      <Route exact path="/" render={(props)=><NewsFeed {...props}/>} />
-                      <Route path="/profile" component={Profile}/>
-                      <Route path="/connections" component={ConnectionsPage}/>
-                      <Route path="/profileAll" component={ () => <FriendsProfilePage/>}/>
-                      <Route path="/post" component={SinglePost}/>
-                      <Route exact path="/chat" render={(props) => <Chat {...props} />} />
-                  </ResultsProvider>
-              </Switch>
-              </div>
-          </BrowserRouter>
-      </div>
-  );
+    return (
+        <div className="container-fluid p-0">
+            <BrowserRouter>
+                <MessengerContext.Provider value={{
+                    koroUser,
+                    setKoroUser,
+                    activeChat,
+                    setActiveChat,
+                    chatMessages,
+                    setChatMessages
+                }}>
+                    <Navbar/>
+                    <div className="row m-0">
+                        <Switch>
+                            <ResultsProvider>
+                                <Redirect from="/index.html" to="/" exact/>
+                                <Route path="/results" component={Search}/>
+                                <Route exact path="/" render={(props) => <NewsFeed {...props}/>}/>
+                                <Route path="/profile" component={Profile}/>
+                                <Route path="/connections" component={ConnectionsPage}/>
+                                <Route path="/profileAll" component={() => <FriendsProfilePage/>}/>
+                                <Route path="/post" component={SinglePost}/>
+                                <Route exact path="/chat" render={(props) => <Chat {...props} />}/>
+                            </ResultsProvider>
+                        </Switch>
+                    </div>
+                </MessengerContext.Provider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
