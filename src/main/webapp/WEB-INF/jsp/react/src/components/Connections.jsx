95b50b20b3ec48460045a6ad0b46cf682e8c5b70
a-koro <korove@hotmail.com>
2021-01-20 19:51:05 +0200

    switched to global state connections and removed connectionsPage component

--- src/main/webapp/WEB-INF/jsp/react/src/components/Connections.jsx
+++ src/main/webapp/WEB-INF/jsp/react/src/components/Connections.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import { ConnectionsContext } from './NewsFeed';
+// import { ConnectionsContext } from './NewsFeed';
 import ConnectionCard from './ConnectionCard';
 import '../css/fixedNavBar.css';
+import GlobalContext from "../contexts/GlobalContext";
 
 function Connections() {
 
-    const friends = React.useContext(ConnectionsContext);
+    // const friends = React.useContext(ConnectionsContext);
+    const {connections} = React.useContext(GlobalContext);
 
     return (
         <>
-            {console.log(friends)}
             <div className="col-sm-6 offset-sm-3 col-xs-12 marginFromTopForFixedNavbar">
                 <h4 className="my-4">
                     Connections
                 </h4>
-                <ConnectionsContext.Consumer>
-                    {(context) => (
-                        context.map((item) => (
-                            <ConnectionCard username={item.firstName + " " + item.lastName} userId={item.userId} />
-                        ))
-                    )}
-                </ConnectionsContext.Consumer>
+                {/*<ConnectionsContext.Consumer>*/}
+                {/*    {(context) => (*/}
+                {/*        context.map((item) => (*/}
+                {/*            <ConnectionCard username={item.firstName + " " + item.lastName} userId={item.userId} />*/}
+                {/*        ))*/}
+                {/*    )}*/}
+                {/*</ConnectionsContext.Consumer>*/}
+                {connections.map((item) => (
+                    <ConnectionCard username={item.firstName + " " + item.lastName} userId={item.userId}/>
+                ))}
             </div>
         </>
     );
